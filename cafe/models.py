65ec85e16d98273ced9e9d7971c7e03558ecc89a
GordPark <gord23p@gmail.com>
2024-04-03 15:54:19 +0900

    cafe 모델 생성

--- cafe/models.py
+++ cafe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# 메뉴 카테고리 : coffe / tea / Beverages
+class Menu_Category(models.Model):
+    category = models.CharField(max_length=20)
+# 메뉴 : 카테고리[Coffe / Tea / Beverage / ] - 메뉴[Americano/Espresso/Latte/Cappuccino/...]
+class Menu(models.Model):
+    menu_text = models.CharField(max_length=20)
+# 옵션: Hot / Iced / Only Iced
+class Menu_Option(models.Model):
+    pass
--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("환영합니다. 무인카페 코코입니다. [주문시작]버튼을 눌러주세요.")
